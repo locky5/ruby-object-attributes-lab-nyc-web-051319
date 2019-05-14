b7a51bc95b53f8d7d84f192da8f5322a394bed6c
Kevin Dai <daikevin19@yahoo.com>
2019-05-14 21:18:53 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name=(the_person_name)
+    @name = the_person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(the_person_job)
+    @job = the_person_job
+  end
+  
+  def job
+    @job
+  end
+end
